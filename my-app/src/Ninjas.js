c198cd817e8a1a83c1033324cf937f50400a6d2c
Rodion <rodion_leginkov@mail.ru>
2020-02-18 17:46:32 +0300

    react_test

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,18 +1,28 @@
 import React from "react";
+import "./Ninjas.css";
 
-const Ninjas = ({ ninjas }) => {
-  {
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>Name: {ninja.name}</div>
-          <div>Age:{ninja.age}</div>
-          <div>Belt: {ninja.belt}</div>
-        </div>
-      );
-    });
-    return <div className="ninja-list">{ninjaList}</div>;
-  }
+const Ninjas = ({ ninjas, deleteNinja }) => {
+  return (
+    <div className="ninja-list">
+      {ninjas.map(ninja => {
+        return (
+          <div className="ninja" key={ninja.id}>
+            <div>Name: {ninja.name}</div>
+            <div>Age: {ninja.age}</div>
+            <div>Belt: {ninja.belt}</div>
+            <button
+              onClick={() => {
+                deleteNinja(ninja.id);
+              }}
+            >
+              Delete ninja
+            </button>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Ninjas;
